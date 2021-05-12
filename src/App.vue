620110b0a67a49a993ce5f543d55a37b9e4426b8
EightDoor <zhou851708184@163.com>
2021-05-12 23:35:29 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>123</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+})
+</script>
+
+<style>
+
+</style>
