01c53605a90bdb0330d1853a80535a0558bd105e
EightDoor <zhou851708184@163.com>
2021-05-18 23:38:34 +0800

    feat: electron 基础构建启动

--- electron/main.js
+++ electron/main.js
@@ -1,23 +1,32 @@
-const { app, BrowserView } = require("electron");
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
-const path = require("path");
-
-function createWindow() {
-  const win = new BrowserView({
+function createWindow () {
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
-  });
-  win.loadFile(path.join(__dirname, "index.html"));
+    // webPreferences: {
+    //   preload: path.join(__dirname, 'preload.js')
+    // }
+  })
+  // win.loadFile(path.join(__dirname, 'dist/index.html'),)
+  win.loadURL("http://localhost:9999/")
+  // 打开调试
+  win.webContents.openDevTools();
 }
 
-app.whenReady().then((res) => {
-  if (BrowserView.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+app.whenReady().then(() => {
+  createWindow()
 
-app.on("window-all-closed", () => {
-  if (process.platform != "darwin") {
-    app.quit();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import styleImport from "vite-plugin-style-import";
 
+
 export default defineConfig({
+  server: {
+    port: 9999,
+  },
+  build: {
+    outDir: "electron/dist",
+  },
   plugins: [
     vue(),
     styleImport({
